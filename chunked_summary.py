fd0a89b9994005bf819d21b4cb92f8698b019fab
Dylan Brackett <dylan.brackett@saildrone.com>
2023-10-31 16:13:39 -0700

    Refactor

--- chunked_summary.py
+++ chunked_summary.py
@@ -1,11 +1,14 @@
 import logging
 import textwrap
+from typing import List, Tuple
 
 from gpt_api_tools import OpenAIConfigHandler
+from gpt_api_tools.ChatCompletionParams import ChatCompletionParams
 from gpt_api_tools.io_utils import safe_read_text, safe_write_json
+from gpt_api_tools.OpenAIClient import OpenAIClient
 
 
-def main() -> None:
+def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
@@ -15,24 +18,22 @@ def main() -> None:
         ],
     )
 
+
+def load_config_and_create_client() -> Tuple[OpenAIClient, ChatCompletionParams]:
     config = OpenAIConfigHandler.load_config("config.json")
-    client, params = OpenAIConfigHandler.create_client_and_params(config)
+    return OpenAIConfigHandler.create_client_and_params(config)
 
-    input_text = safe_read_text("input.txt")
-    if not input_text:
-        logging.error("Input file is empty. Exiting...")
-        return
 
-    chunks = textwrap.wrap(input_text, 4096)
+def chunk_text(input_text: str) -> List[str]:
+    return textwrap.wrap(input_text, 4096)
 
-    logging.info(f"Number of chunks: {len(chunks)}")
-    input("Press Enter to continue...")
 
-    system_prompt = safe_read_text("prompt.txt")
-    if not system_prompt:
-        logging.error("Prompt file is empty. Exiting...")
-        return
-
+def summarize_text(
+    client: OpenAIClient,
+    params: ChatCompletionParams,
+    chunks: List[str],
+    system_prompt: str,
+) -> List[str]:
     results = []
     for count, chunk in enumerate(chunks, start=1):
         messages = [
@@ -47,6 +48,29 @@ def main() -> None:
         logging.info(f"{completion}\n\n")
         results.append(completion)
         logging.info(f"Chunk {count} of {len(chunks)} completed\n\n")
+    return results
+
+
+def main() -> None:
+    setup_logging()
+
+    client, params = load_config_and_create_client()
+
+    input_text = safe_read_text("input.txt")
+    if not input_text:
+        logging.error("Input file is empty. Exiting...")
+        return
+
+    chunks = chunk_text(input_text)
+    logging.info(f"Number of chunks: {len(chunks)}")
+    input("Press Enter to continue...")
+
+    system_prompt = safe_read_text("prompt.txt")
+    if not system_prompt:
+        logging.error("Prompt file is empty. Exiting...")
+        return
+
+    results = summarize_text(client, params, chunks, system_prompt)
 
     if not safe_write_json("output.json", results):
         logging.error("Failed to write output to file. Exiting...")
